b0e429929d4a4bb18da9d46887f5e97ae31942d5
WhatAKitty <104xuqiang@163.com>
2017-10-04 22:20:49 +0800

    Using custom router for the whole lifecycle of navigation

---
+++ src/router.js
@@ -0,0 +1,62 @@
+
+import { NavigationActions } from 'react-navigation';
+
+// gets the current screen from navigation state
+const getCurrentRoute = (navigationState) => {
+  if (!navigationState) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  // dive into nested navigators
+  if (route.routes) {
+    return getCurrentRoute(route);
+  }
+  return route;
+}
+
+const _routes = Symbol('routes');
+const _current = Symbol('_current');
+
+class Router {
+  constructor() {
+    this[_routes] = [];
+    this[_current] = -1;
+  }
+
+  get first() {
+    return this[_current] === 0 || this[_current] === -1;
+  }
+
+  get prev() {
+    if (this.first) return null;
+    return this[_routes][this[_current] - 1];
+  }
+
+  navigate(navigation, routeName, params, action) {
+    this[_routes].push({
+      routeName,
+      params,
+    });
+    navigation.dispatch(
+      NavigationActions.navigate({
+        routeName,
+        params,
+        action,
+      })
+    );
+    console.log(this[_routes])
+  }
+
+  goBack(navigation, key) {
+    if (!key) key = this.prev && this.prev.routeName;
+    this[_routes].pop();
+    navigation.dispatch(
+      NavigationActions.back({
+        key: key === undefined ? navigation.state.key : key,
+      })
+    )
+  }
+
+}
+
+export default Router;

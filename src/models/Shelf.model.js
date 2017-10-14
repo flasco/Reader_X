821fcc8b9d2a9dd8e9e493510d1f56748d7e890e
WhatAKitty <104xuqiang@163.com>
2017-10-14 20:09:20 +0800

    Add properties LastUpdateChapterName for Shelf schema

--- src/models/Shelf.model.js
+++ src/models/Shelf.model.js
@@ -6,15 +6,16 @@ Shelf.schema = {
   name: 'Shelf',
   primaryKey: 'BookId',
   properties: {
-    BookId:               'int',                                                      // 图书编号
-    BookName:             'string',                                                   // 图书名称
-    AuthorId:             'int',                                                      // 图书作者编号
-    Author:               'string',                                                   // 图书作者
-    Description:          'string',                                                   // 图书描述
-    Label:                {type: 'string', optional: true},                           // 图书tag
-    LastReadedTime:       {type: 'int', optional: true, indexed: true},               // 上次阅读时间，时间戳
-    LastChapter:          {type: 'string', optional: true},                           // 最近阅读章节
-    Progress:             {type: 'double', optional: true},                           // 阅读进度
+    BookId:                   'int',                                                      // 图书编号
+    BookName:                 'string',                                                   // 图书名称
+    AuthorId:                 'int',                                                      // 图书作者编号
+    Author:                   'string',                                                   // 图书作者
+    Description:              'string',                                                   // 图书描述
+    Label:                    {type: 'string', optional: true},                           // 图书tag
+    LastUpdateChapterName:    'string',                                                   // 最新更新章节名称
+    LastReadedTime:           {type: 'int', optional: true, indexed: true},               // 上次阅读时间，时间戳
+    LastChapter:              {type: 'string', optional: true},                           // 最近阅读章节
+    Progress:                 {type: 'double', optional: true},                           // 阅读进度
   },
 }
 
